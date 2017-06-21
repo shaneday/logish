919279da3fd69ebbb78a9d6fdc2eed70033b0386
Shane Day <shane@itlab.co.nz>
2017-06-21 21:59:11 +1200

    Initial commit

---
+++ logish.go
@@ -0,0 +1,66 @@
+package logish
+
+import "fmt"
+
+type fieldItem struct {
+	label   string
+	vformat string
+	value   interface{}
+}
+type Logger struct {
+	messages []string
+	fields   []fieldItem
+}
+
+func (l *Logger) Logf(format string, a ...interface{}) {
+	if l == nil {
+		return
+	}
+	str := fmt.Sprintf(format, a...)
+	l.messages = append(l.messages, str)
+}
+
+func (l *Logger) Field(label string, a interface{}) {
+	if l == nil {
+		return
+	}
+	e := fieldItem{label: label, value: a}
+	l.fields = append(l.fields, e)
+}
+func (l *Logger) Fieldf(label, vformat string, a interface{}) {
+	if l == nil {
+		return
+	}
+	e := fieldItem{label: label, vformat: vformat, value: a}
+	l.fields = append(l.fields, e)
+}
+
+func (l *Logger) Clear() {
+	if l == nil {
+		return
+	}
+	l.messages = []string{}
+	l.fields = []fieldItem{}
+}
+
+func (l *Logger) Exit() {
+	if l == nil {
+		return
+	}
+	for _, str := range l.messages {
+		fmt.Println(str)
+	}
+	maxWidth := 0
+	for _, e := range l.fields {
+		if len(e.label) > maxWidth {
+			maxWidth = len(e.label)
+		}
+	}
+	for _, e := range l.fields {
+		if e.vformat == "" {
+			e.vformat = "%#v"
+		}
+		val := fmt.Sprintf(e.vformat, e.value)
+		fmt.Printf("%-*s %s\n", maxWidth+1, e.label+":", val)
+	}
+}
---
+++ logish_test.go
@@ -0,0 +1,59 @@
+package logish
+
+import "fmt"
+
+func ExampleLogf() {
+	l := Logger{}
+	defer l.Exit()
+	l.Logf("Hello %d", 1)
+	l.Logf("Hello %d", 2)
+	fmt.Println("Println call")
+	// Output:
+	// Println call
+	// Hello 1
+	// Hello 2
+}
+
+func ExampleField() {
+	l := Logger{}
+	defer l.Exit()
+	l.Field("field1", 1)
+	l.Field("field2withLongName", 1.2)
+	l.Field("field3", "abc")
+	// Output:
+	// field1:             1
+	// field2withLongName: 1.2
+	// field3:             "abc"
+}
+
+func ExampleFieldF() {
+	l := Logger{}
+	defer l.Exit()
+	l.Fieldf("field1", "%2.3f", 1.2)
+	l.Fieldf("field2", "%#[1]x %[1]d", 123)
+	// Output:
+	// field1: 1.200
+	// field2: 0x7b 123
+}
+
+func ExampleClear() {
+	l := Logger{}
+	defer l.Exit()
+	l.Logf("log1")
+	l.Field("field1", 1)
+	l.Fieldf("field2", "%x", 2)
+	l.Clear()
+	l.Logf("log2")
+	// Output:
+	// log2
+}
+
+func ExampleNilLogger() {
+	var l *Logger = nil
+	defer l.Exit()
+	l.Field("field1", 1)
+	l.Fieldf("field1", "%d", 1)
+	l.Logf("log1")
+	l.Clear()
+	// Output:
+}
